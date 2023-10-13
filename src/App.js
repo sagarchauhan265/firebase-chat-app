13b6f168c64cc01276612a3c5115cdc88489e00d
sagarchauhan265 <sagarchauhan265@gmail.com>
2023-10-14 04:17:02 +0530

    chat start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React, { useState } from 'react';
+import { Container, Paper, TextField, Button, List, ListItem, ListItemText } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const handleSendMessage = () => {
+    if (message.trim() !== '') {
+      setMessages([...messages, { text: message, sender: 'You' }]);
+      setMessage('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxWidth="sm">
+      <Paper elevation={3} style={{ padding: '20px', height: '400px', overflowY: 'auto' }}>
+        <List>
+          {messages.map((msg, index) => (
+            <ListItem key={index}>
+              <ListItemText
+                primary={msg.sender}
+                secondary={msg.text}
+                primaryTypographyProps={{ variant: 'subtitle2' }}
+                secondaryTypographyProps={{ variant: 'body1' }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+      <div style={{ display: 'flex', marginTop: '10px' }}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Type your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          endIcon={<SendIcon />}
+          onClick={handleSendMessage}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Send
+        </Button>
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
+
